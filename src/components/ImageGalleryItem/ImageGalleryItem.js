7985adb042c96e2add6774cebaecb04e14e4504c
htmldiana <dianekhomenko@gmail.com>
2023-04-26 21:55:28 +0200

    modal

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -2,14 +2,34 @@ import {
   GalleryItem,
   GalleryItemImage,
 } from 'components/ImageGalleryItem/ImageGalleryItem.styled';
-import { Modal } from 'components/Modal/Modal'
+import { ModalItem } from 'components/Modal/Modal';
+import Modal from 'react-modal';
+import React from 'react';
 
 export const ImageGalleryItem = ({ smallImage, bigImage }) => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <GalleryItem>
-      <GalleryItemImage src={smallImage} alt="" />
-      <Modal bigImage={bigImage} />
-      {console.log(this.state.isLoading)}
+      <GalleryItemImage src={smallImage} alt="" onClick={openModal} />
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        shouldCloseOnOverlayClick={true}
+        contentLabel="Photo Modal"
+        ariaHideApp={false}
+      >
+        <ModalItem bigImage={bigImage} />
+      </Modal>
     </GalleryItem>
   );
 };
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,11 @@
-import { Overlay, ModalPhoto} from 'components/Modal/Modal.styled';
+import { Overlay, ModalPhoto } from 'components/Modal/Modal.styled';
 
-export const Modal = ({bigImage}) => {
-
-    return (
-      <Overlay>
-        <ModalPhoto>
-          <img src={bigImage} alt="" />
-        </ModalPhoto>
-      </Overlay>
-    );
+export function ModalItem({ bigImage }) {
+  return (
+    <Overlay>
+      <ModalPhoto>
+        <img src={bigImage} alt="" />
+      </ModalPhoto>
+    </Overlay>
+  );
 }
