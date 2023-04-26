7b4ef7ef2623a784accac237807ce3d9fd83331f
htmldiana <dianekhomenko@gmail.com>
2023-04-26 20:16:24 +0200

    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { Layout } from 'components/Layout.styled';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { Button } from 'components/Button/Button';
-import { func } from 'prop-types';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '34409732-2eb98e59aad866aa53f09776f';
@@ -26,11 +25,20 @@ export class App extends Component {
 
   onLoad = e => {
     console.log('load');
-
     this.setState(prevState => {
       return { page: prevState.page + 1 };
     });
+    this.loadPage();
   };
+
+  async loadPage() {
+    const response = await axios.get(
+      `?q=${this.state.search}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    this.setState(prevState => {
+      return { photos: [...prevState.photos, ...response.data.hits] };
+    });
+  }
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevState.search !== this.state.search) {
