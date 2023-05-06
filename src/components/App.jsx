c5058c93ad8c0c79b239d49859fe28119ba0e2d5
htmldiana <dianekhomenko@gmail.com>
2023-05-06 21:46:33 +0200

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Searchbar } from './Search/Searchbar';
 import axios from 'axios';
 import { Layout } from 'components/Layout.styled';
@@ -9,76 +9,64 @@ import { Circles } from 'react-loader-spinner';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '34409732-2eb98e59aad866aa53f09776f';
 
-export class App extends Component {
-  state = {
-    search: '',
-    photos: [],
-    page: 1,
-    isLoading: false,
-  };
+export const App = () => {
+  const [search, setSearch] = useState('');
+  const [photos, setPhotos] = useState([]);
+  const page = useRef(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-  onSearch = e => {
+  useEffect(() => {
+    if (search !== '') {
+      async function getData() {
+        setIsLoading(true);
+        const response = await axios.get(
+          `?q=${search}&page=${page.current}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+        );
+        setPhotos(response.data.hits);
+      }
+
+      getData();
+      page.current += 1;
+      setIsLoading(false);
+    }
+  }, [search]);
+
+  const onSearch = e => {
     e.preventDefault();
-    this.setState({
-      search: e.currentTarget.elements.searchbar.value,
-      page: 1,
-    });
+    setSearch(e.currentTarget.elements.searchbar.value);
+    page.current = 1;
     e.currentTarget.elements.searchbar.value = '';
   };
 
-  onLoad = e => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-    this.loadPage();
+  const onLoad = () => {
+    page.current += 1;
+
+    setIsLoading(true);
+    async function getData() {
+      const response = await axios.get(
+        `?q=${search}&page=${page.current}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      );
+      setPhotos(prevState => [...prevState, ...response.data.hits]);
+    }
+    getData();
+    setIsLoading(false);
   };
 
-  async loadPage() {
-    this.setState({
-      isLoading: true,
-    });
-    const response = await axios.get(
-      `?q=${this.state.search}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    );
-    this.setState(prevState => {
-      return {
-        photos: [...prevState.photos, ...response.data.hits],
-        isLoading: false,
-      };
-    });
-  }
-
-  async componentDidUpdate(prevProps, prevState) {
-    if (prevState.search !== this.state.search) {
-      this.setState({ isLoading: true });
-      const response = await axios.get(
-        `?q=${this.state.search}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      );
-      this.setState({
-        photos: response.data.hits,
-        page: 2,
-        isLoading: false,
-      });
-    }
-  }
-
-  render() {
-    return (
-      <Layout>
-        <Searchbar onSubmit={this.onSearch} />
-        {this.state.isLoading && (
-          <Circles
-            height="80"
-            width="80"
-            color="#3f51b5"
-            ariaLabel="circles-loading"
-            wrapperClass="loader"
-            visible={true}
-          />
-        )}
-        <ImageGallery photos={this.state.photos} />
-        {this.state.photos.length > 0 && <Button onLoad={this.onLoad} />}
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <Searchbar onSubmit={onSearch} />
+      {isLoading && (
+        <Circles
+          height="80"
+          width="80"
+          color="#3f51b5"
+          ariaLabel="circles-loading"
+          wrapperClass="loader"
+          visible={true}
+        />
+      )}
+      <ImageGallery photos={photos} />
+      {photos.length > 0 && <Button onLoad={onLoad} />}
+    </Layout>
+  );
+};
